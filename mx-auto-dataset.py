7899c247ec5e92ff534d5bb9995e367849036c5a
Jun Aishima <jun.aishima@synchrotron.org.au>
2015-12-23 10:43:45 +1100

    give new name to script instead of testing

---
+++ mx-auto-dataset.py
@@ -0,0 +1,14 @@
+from beamline import redis
+from rq.decorators import job
+from subprocess import call
+
+@job('autodatasetdev', connection=redis, timeout=1800)
+def dataset(*args, **kwargs):
+    print "RUNNING NEW AUTODATASET"
+    cmd = ['/beamline/apps/mx-auto-dataset/autodataset.sh']
+
+    for key, value in kwargs.iteritems():
+        cmd.append("--%s" % key)
+        cmd.append(str(value))
+
+    call(cmd, cwd='/ssd/test')
