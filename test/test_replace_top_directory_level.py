dc6f1100bfceb140559fa7922d5a4d092bb14b51
Jun Aishima <jun.aishima@synchrotron.org.au>
2016-01-06 12:33:11 +1100

    make test unittest compatible
     * it passes!

--- test/test_replace_top_directory_level.py
+++ test/test_replace_top_directory_level.py
@@ -2,20 +2,20 @@ __author__ = 'aishimaj'
 
 from utils import replace_top_directory_level
 
-def test1():
-    test_dir = "/data/10123g/frames/calibration/test_crystal/testcrystal_1_180.img"
-    data_dir = "/sans/60days"
-    result = replace_top_directory_level(test_dir, data_dir)
-    assert result == "/sans/60days/10123g/frames/calibration/test_crystal/testcrystal_1_180.img"
+import unittest
 
-def test2():
-    test_dir = "/data/10123g/frames/calibration/test_crystal/testcrystal_1_180.img"
-    data_dir = "/sans/60days/"
-    try:
+class TestReplaceTopDirectoryLevel(unittest.TestCase):
+    def test1(self):
+        test_dir = "/data/10123g/frames/calibration/test_crystal/testcrystal_1_180.img"
+        data_dir = "/sans/60days"
         result = replace_top_directory_level(test_dir, data_dir)
-    except ValueError, e:
-        pass #this is the expected behavior
-    raise Exception("failed")
+        self.assertTrue(result == "/sans/60days/10123g/frames/calibration/test_crystal/testcrystal_1_180.img")
 
-test1()
-test2()
+    def test2(self):
+        test_dir = "/data/10123g/frames/calibration/test_crystal/testcrystal_1_180.img"
+        data_dir = "/sans/60days/"
+        with self.assertRaises(ValueError):
+            result = replace_top_directory_level(test_dir, data_dir)
+
+if __name__ == '__main__':
+    unittest.main()
