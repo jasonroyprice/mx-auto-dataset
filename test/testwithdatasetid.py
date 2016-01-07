a5f7ff6d3339a5eda0a827fa3d3cd800d772a492
Jun Aishima <jun.aishima@synchrotron.org.au>
2016-01-07 14:49:50 +1100

    add the rest of the arguments to the test runner

--- test/testwithdatasetid.py
+++ test/testwithdatasetid.py
@@ -13,9 +13,25 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--dataset_id", help="dataset ID")
 parser.add_argument("--data_dir", help="top-level directory name (like /data for most data)")
 parser.add_argument("--output_dir", help="place where processing results are put")
+parser.add_argument('--unitcell', default='')
+parser.add_argument('--spacegroup', default='')
+parser.add_argument('--first_frame', default='')
+parser.add_argument('--last_frame', default='')
+parser.add_argument('--low_resolution', default='')
+parser.add_argument('--high_resolution', default='')
+parser.add_argument('--ice', default='')
+parser.add_argument('--weak', default='')
+parser.add_argument('--slow', default='')
+parser.add_argument('--brute', default='')
+
 args = parser.parse_args()
 if not args.dataset_id:
     import sys
     sys.exit("no dataset ID provided, aborting")
-job = dataset.delay(dataset_id=unicode(args.dataset_id), data_dir=unicode(args.data_dir), output_dir=unicode(args.output_dir))
+job = dataset.delay(dataset_id=unicode(args.dataset_id), data_dir=unicode(args.data_dir),
+                    output_dir=unicode(args.output_dir), unitcell=unicode(args.unitcell),
+                    spacegroup=unicode(args.spacegroup), first_frame=unicode(args.first_frame),
+                    last_frame=unicode(args.last_frame), low_resolution=unicode(args.low_resolution),
+                    high_resolution=unicode(args.high_resolution), weak=unicode(args.weak), slow=unicode(args.slow),
+                    brute=unicode(args.brute))
 logger.info('Queued dataset job with id = %s' % (job,))
