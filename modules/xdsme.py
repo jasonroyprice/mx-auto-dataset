55c2b02fb5d01a4c49f0e791b24ff62275dae09f
Jun Aishima <jun.aishima@synchrotron.org.au>
2017-01-16 14:30:39 +1100

    modify arguments for indexing on cpu2 - no nice to prioritize over processing

--- modules/xdsme.py
+++ modules/xdsme.py
@@ -96,7 +96,7 @@ class XDSme(Base):
         self.move_files()
 
     def run_xdsme(self, extra):
-        args = ['xdsme']
+        args = ['numactl', '--cpunodebind=2', 'xdsme', '-n', '40', '--invert']
         args.extend(['-p', self.output.project])
         args.extend(['-i', ' '.join(self.XDS_INPUT)])
         if self.p1:
