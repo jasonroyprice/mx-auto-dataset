de4621c299ae2667331be501338f92e47667243e
Jun Aishima <jun.aishima@synchrotron.org.au>
2017-02-09 15:26:58 +1100

    remove invert - we're the right way around again

--- modules/xdsme.py
+++ modules/xdsme.py
@@ -96,7 +96,7 @@ class XDSme(Base):
         self.move_files()
 
     def run_xdsme(self, extra):
-        args = ['nice', 'numactl', '--cpunodebind=2', 'xdsme', '-n', '40', '--invert']
+        args = ['nice', 'numactl', '--cpunodebind=2', 'xdsme', '-n', '40']
         args.extend(['-p', self.output.project])
         args.extend(['-i', ' '.join(self.XDS_INPUT)])
         if self.p1:
