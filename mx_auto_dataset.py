ed64f18e539089575c2e52c12ccc84d866aa2a69
Jun Aishima <jun.aishima@synchrotron.org.au>
2017-09-04 13:47:19 +1000

    allow printing of current git revision of pipeline code

--- mx_auto_dataset.py
+++ mx_auto_dataset.py
@@ -1,12 +1,19 @@
 from beamline import redis
 from rq.decorators import job
-from subprocess import call
+from subprocess import call, Popen
+import subprocess
 import config
+import os
 
 @job(config.REDIS_QUEUE_NAME, connection=redis, timeout=1800)
 def dataset(*args, **kwargs):
-    print "RUNNING %s AUTODATASET" % config.PIPELINE_NAME
-    cmd = ['/xray/progs/Python/libraries/mx_auto_dataset/autodataset.sh']
+    pipeline_path = '/xray/progs/Python/libraries/mx_auto_dataset/autodataset.sh'
+    dir_path = os.path.dirname(os.path.realpath(pipeline_path))
+    os.chdir(dir_path)
+    output = Popen(['git', 'rev-parse', 'HEAD'], stdout=subprocess.PIPE)
+    version = output.stdout.read()[0:7]
+    print "RUNNING %s AUTODATASET VERSION %s" % (config.PIPELINE_NAME, version)
+    cmd = [pipeline_path]
 
     for key, value in kwargs.iteritems():
         cmd.append("--%s" % key)
