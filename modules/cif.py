ca17a65d7b97ffd6eddeb6a9f094be44ecdc0c1d
Jun Aishima <jun.aishima@synchrotron.org.au>
2018-05-16 13:52:36 +1000

    MXPROC-11 add initial template and script to generate a sample template

---
+++ modules/cif.py
@@ -0,0 +1,30 @@
+from jinja2 import Environment, FileSystemLoader
+
+#incoming parameters
+beamline = 'MX2' # beamline in collection objecxt
+energy_in_kev = 13.25
+detector = 'eiger' #detector_type in collection object
+cryojet_temperature = 110.2
+
+if beamline == 'MX1':
+    beamline_text = 'MX1 Beamline Australian Synchrotron'
+elif beamline == 'MX2':
+    beamline_text = 'MX2 Beamline Australian Synchrotron'
+else:
+    raise Exception('Unknown beamline')
+
+if detector == 'eiger':
+    detector_text = 'Eiger 16M'
+elif detector == 'adsc':
+    deetector_text = 'ADSC q215'
+else:
+    raise Exception('Unknown detector')
+
+env = Environment(
+    loader=FileSystemLoader('templates'))
+
+template = env.get_template('cx_template.cif')
+KEV_TO_ANGSTROM = 12.398420
+energy = energy_in_kev/KEV_TO_ANGSTROM
+
+print template.render(detector=detector_text, beamline=beamline_text, energy=energy, temperature=cryojet_temperature)
