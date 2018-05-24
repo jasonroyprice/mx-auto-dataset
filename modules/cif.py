fc71ca171c6617570029eb1def4d3c3250910975
Jun Aishima <jun.aishima@synchrotron.org.au>
2018-05-24 17:12:45 +1000

    MXPROC-11 modularize cif creation
     * add test that can be run from mx_auto_dataset level

--- modules/cif.py
+++ modules/cif.py
@@ -1,33 +1,37 @@
 from jinja2 import Environment, FileSystemLoader
 
-#incoming parameters
-beamline = 'MX2' # beamline in collection objecxt
-energy_in_kev = 13.25
-detector = 'eiger' #detector_type in collection object
-cryojet_temperature = 110.2
-project_dir = '.'
+def run_test():
+    beamline = 'MX2' # beamline in collection objecxt
+    energy_in_kev = 13.25
+    detector = 'eiger' #detector_type in collection object
+    cryojet_temperature = 110.2
+    project_dir = '.'
+    write_template_file(project_dir, beamline, detector, energy_in_kev, cryojet_temperature)
 
-if beamline == 'MX1':
-    beamline_text = 'MX1 Beamline Australian Synchrotron'
-elif beamline == 'MX2':
-    beamline_text = 'MX2 Beamline Australian Synchrotron'
-else:
-    raise Exception('Unknown beamline')
+def write_template_file(project_dir, beamline, detector, energy_in_kev, cryojet_temperature):
+    if beamline == 'MX1':
+        beamline_text = 'MX1 Beamline Australian Synchrotron'
+    elif beamline == 'MX2':
+        beamline_text = 'MX2 Beamline Australian Synchrotron'
+    else:
+        raise Exception('Unknown beamline')
 
-if detector == 'eiger':
-    detector_text = 'Eiger 16M'
-elif detector == 'adsc':
-    deetector_text = 'ADSC q215'
-else:
-    raise Exception('Unknown detector')
+    if detector == 'eiger':
+        detector_text = 'Eiger 16M'
+    elif detector == 'adsc':
+        detector_text = 'ADSC q215'
+    else:
+        raise Exception('Unknown detector')
 
-env = Environment(
-    loader=FileSystemLoader('templates'))
+    env = Environment(
+        loader=FileSystemLoader('templates'))
 
-template = env.get_template('cx_template.cif')
-KEV_TO_ANGSTROM = 12.398420
-energy = energy_in_kev/KEV_TO_ANGSTROM
+    template = env.get_template('cx_template.cif')
+    KEV_TO_ANGSTROM = 12.398420
+    energy = energy_in_kev/KEV_TO_ANGSTROM
 
-with open('%s/%s' % (project_dir, 'template.cif'), 'w') as template_file:
-    template_file.write(template.render(detector=detector_text, beamline=beamline_text, energy=energy, temperature=cryojet_temperature))
+    with open('%s/%s' % (project_dir, 'template.cif'), 'w') as template_file:
+        template_file.write(template.render(detector=detector_text, beamline=beamline_text, energy=energy, temperature=cryojet_temperature))
 
+if __name__ == '__main__':
+    run_test()
