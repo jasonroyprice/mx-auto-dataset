618f713ec7292859374906a7c65dd3b604dcc57a
katesmith280 <51903787+katesmith280@users.noreply.github.com>
2019-07-03 11:49:08 +1000

    Update CX_xprep_graphs.py
    Checking if the statistics table we extract for graphs exists in the XDS_ASCII_p1.prp file.
    If it doesn't exist in the file - pass
    Else it does exist in the file - continue to extract data and generate the graphs

--- modules/CX_xprep_graphs.py
+++ modules/CX_xprep_graphs.py
@@ -26,65 +26,69 @@ def xprep_graphs(project_dir,filename):
     # Search file for beginning and ending of stats table
     x = re.search(" Inf",contents)
     y = re.search("Merged",contents)
-    stats_table = contents[x.start():y.start()].split('\n')
+    
+    if x is None and y is None:
+        pass
+    else:
+        stats_table = contents[x.start():y.start()].split('\n')
 
-    # Split the rows of the stats table into individual elements
-    n = 0
-    data = []
-    for item in stats_table:
-        data.append(stats_table[n].split())
-        n+=1
+        # Split the rows of the stats table into individual elements
+        n = 0
+        data = []
+        for item in stats_table:
+            data.append(stats_table[n].split())
+            n+=1
 
-    # Store the stats table as a pandas dataframe and remove last 5 rows (are empty or contain punctuation)
-    df = pd.DataFrame(data,columns=['Resolution', 'dash', 'Resolution High',
-                                    '#Data','#Theory','%Complete', 'Redundancy',
-                                    'Mean I','Mean I/s','R(int)','Rsigma'],dtype=float)
-    df.drop(df.tail(5).index,inplace=True)
+        # Store the stats table as a pandas dataframe and remove last 5 rows (are empty or contain punctuation)
+        df = pd.DataFrame(data,columns=['Resolution', 'dash', 'Resolution High',
+                                        '#Data','#Theory','%Complete', 'Redundancy',
+                                        'Mean I','Mean I/s','R(int)','Rsigma'],dtype=float)
+        df.drop(df.tail(5).index,inplace=True)
 
-    x = df['Resolution High']
+        x = df['Resolution High']
 
-    y1 = df['R(int)']
-    y2 = df['Rsigma']
-    y3 = df['%Complete']
-    y4 = df['Mean I/s']
+        y1 = df['R(int)']
+        y2 = df['Rsigma']
+        y3 = df['%Complete']
+        y4 = df['Mean I/s']
 
 
-    #Graph the following using matplotlib and numpy
+        #Graph the following using matplotlib and numpy
 
-    grid = plt.GridSpec(2, 2, wspace=0.2, hspace=0.3)
+        grid = plt.GridSpec(2, 2, wspace=0.2, hspace=0.3)
 
-    plt.subplot(grid[0,0])
-    plt.title('Resolution vs R(int)')
-    plt.plot(x, y1, '-',color='m')
-    plt.xlabel('Resolution')
-    plt.ylabel('R(int)')
-    plt.gca().invert_xaxis()
+        plt.subplot(grid[0,0])
+        plt.title('Resolution vs R(int)')
+        plt.plot(x, y1, '-',color='m')
+        plt.xlabel('Resolution')
+        plt.ylabel('R(int)')
+        plt.gca().invert_xaxis()
 
-    plt.subplot(grid[0,1])
-    plt.title('Resolution vs Rsigma')
-    plt.plot(x, y2, '-',color='r')
-    plt.xlabel('Resolution')
-    plt.ylabel('Rsigma')
-    plt.gca().invert_xaxis()
+        plt.subplot(grid[0,1])
+        plt.title('Resolution vs Rsigma')
+        plt.plot(x, y2, '-',color='r')
+        plt.xlabel('Resolution')
+        plt.ylabel('Rsigma')
+        plt.gca().invert_xaxis()
 
-    plt.subplot(grid[1,0])
-    plt.title('Resolution vs Completeness')
-    plt.plot(x, y3, '-',color='g')
-    plt.xlabel('Resolution')
-    plt.ylabel('Completeness (%)')
-    plt.gca().invert_xaxis()
+        plt.subplot(grid[1,0])
+        plt.title('Resolution vs Completeness')
+        plt.plot(x, y3, '-',color='g')
+        plt.xlabel('Resolution')
+        plt.ylabel('Completeness (%)')
+        plt.gca().invert_xaxis()
 
-    plt.subplot(grid[1,1])
-    plt.title('Resolution vs Mean I/s')
-    plt.plot(x, y4, '-',color='b')
-    plt.xlabel('Resolution')
-    plt.ylabel('Mean I/s')
-    plt.gca().invert_xaxis()
+        plt.subplot(grid[1,1])
+        plt.title('Resolution vs Mean I/s')
+        plt.plot(x, y4, '-',color='b')
+        plt.xlabel('Resolution')
+        plt.ylabel('Mean I/s')
+        plt.gca().invert_xaxis()
 
-    plt.subplots_adjust(bottom=0.1,right=2,top=2)
+        plt.subplots_adjust(bottom=0.1,right=2,top=2)
 
-    # Save the .png graph file into the current directory
-    plt.savefig(os.path.join(project_dir,filename.split(".")[0]+'.png'),bbox_inches='tight',pad_inches=0.1)
+        # Save the .png graph file into the current directory
+        plt.savefig(os.path.join(project_dir,filename.split(".")[0]+'.png'),bbox_inches='tight',pad_inches=0.1)
 
 class XprepGraphs(Base):
 
